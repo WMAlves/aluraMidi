892736d0a8261d0726639ae95082da8db0e09850
WMAlves <williamma03@gmail.com>
2022-06-20 00:24:18 -0300

    Aprendendo a usar listas
    Aprendendo a usar lista nessa aula

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 function tocaSomPom(){
         document.querySelector('#som_tecla_pom').play();
-        document.querySelector('.tecla_pom').onclick = tocaSomPom;
-    }
-    
+        
+    }   
+
+    const listaDeTeclas = document.querrySelectorAll('.tecla');
+        
+//https://cursos.alura.com.br/course/javascript-web-paginas-dinamicas/task/97753
+//rever aula
+
+  
     
 
